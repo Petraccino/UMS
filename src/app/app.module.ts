05ba5474eca09e804ad73653477208529706cab5
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2023-12-13 12:37:12 +0100

    Integration push
    Routing setting - component users - insert bootstrap dependencies

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app.routes';
+import { UsersComponent } from './users/users.component';
+
+@NgModule({
+  declarations: [AppComponent, UsersComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/app/users/users.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { UserService } from '../service/user.service';
+
+@Component({
+  selector: 'app-users',
+  providers: [],
+  templateUrl: 'users.component.html',
+  styleUrls: ['users.component.css'],
+})
+export class UsersComponent {
+  title = 'Users';
+  public users: any[] = [];
+  constructor(private service: UserService) {
+    this.users = service.getUsers();
+  }
+}
