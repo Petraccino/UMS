d0671c20c3fb5d971dba73dc82560c45b9157d78
Marco Petraccini <129108720+Petraccino@users.noreply.github.com>
2023-12-23 11:39:39 +0100

    Modify user service for interrogation laravel app

--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -2,7 +2,19 @@ import { HttpClient } from '@angular/common/http';
 import { User } from './../classes/user';
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+
+interface UsersResponse {
+  body: User[];
+  message: string;
+  success: boolean;
+}
+
+interface UserResponse {
+  body: User;
+  message: string;
+  success: boolean;
+}
 
 @Injectable({
   providedIn: 'root',
@@ -11,22 +23,32 @@ export class UserService {
   apiurl = environment.API_URL;
 
   constructor(private http: HttpClient) {}
-  
+
   getUsers(): Observable<User[]> {
-    return this.http.get<User[]>(this.apiurl);
+    return this.http
+      .get<UsersResponse>(this.apiurl)
+      .pipe(map((resp) => resp.body));
   }
   getUser(id: number): Observable<User> {
-    return this.http.get<User>(this.apiurl + '/' + id);
+    return this.http
+      .get<UserResponse>(this.apiurl + '/' + id)
+      .pipe(map((resp) => resp.body));
   }
   deleteUser(user: User): Observable<User> {
-    return this.http.delete<User>(this.apiurl + '/' + user.id);
+    return this.http
+      .delete<UserResponse>(this.apiurl + '/' + user.id)
+      .pipe(map((resp) => resp.body));
   }
 
   updateUser(user: User): Observable<User> {
-    return this.http.put<User>(this.apiurl + '/' + user.id, user);
+    return this.http
+      .put<UserResponse>(this.apiurl + '/' + user.id, user)
+      .pipe(map((resp) => resp.body));
   }
 
   createUser(user: User): Observable<User> {
-    return this.http.post<User>(this.apiurl, user);
+    return this.http
+      .post<UserResponse>(this.apiurl, user)
+      .pipe(map((resp) => resp.body));
   }
 }
